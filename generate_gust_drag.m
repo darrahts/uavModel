3e13f1e3872820afef231842977c1567c22ffaba
darrahts <timothy.s.darrah@vanderbilt.edu>
2019-03-04 18:05:18 -0600

    added function to generate drag from wind gusts

---
+++ generate_gust_drag.m
@@ -0,0 +1,20 @@
+function drag = generate_gust_drag(gusts)
+%
+% creates drag from wind gusts
+%
+% input: gusts (x,y,z)
+%
+% output: drag (x,y,z)
+%
+
+areas = [.0197 .0197 .512];
+drag = [0 0 0];
+rho = 1.225;
+coef_d = 1.0;
+
+for i=1:3
+    drag(i) = -.5*rho*gusts(i)^2*areas(i)*coef_d;
+    if(gusts>0)
+        drag(i) = drag(i) * -1;
+    end   
+end
