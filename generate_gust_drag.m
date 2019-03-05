b8bb76f63e3cbafcc5f99e710663ab644fd46c45
darrahts <timothy.s.darrah@vanderbilt.edu>
2019-03-04 20:38:09 -0600

    fixed indexing error

--- generate_gust_drag.m
+++ generate_gust_drag.m
@@ -7,14 +7,14 @@ function drag = generate_gust_drag(gusts)
 % output: drag (x,y,z)
 %
 
-areas = [.0197 .0197 .512];
+areas = [.0197 .0197 .0197*2*1.3];
 drag = [0 0 0];
 rho = 1.225;
 coef_d = 1.0;
 
 for i=1:3
     drag(i) = -.5*rho*gusts(i)^2*areas(i)*coef_d;
-    if(gusts>0)
+    if(gusts(i)>0)
         drag(i) = drag(i) * -1;
     end   
 end
