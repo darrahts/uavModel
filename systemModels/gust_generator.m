3ba68f02d568d4d3fed434b153824c51501847be
darrahts <timothy.s.darrah@vanderbilt.edu>
2019-04-01 17:46:33 -0500

    updated wind and gusts

--- systemModels/gust_generator.m
+++ systemModels/gust_generator.m
@@ -1,25 +1,26 @@
-function gusts = gust_generator(period, amplitude, t)
+function gusts = gust_generator(period, amplitude, enabled, t)
 
 gusts = [0,0,0];
 idx = 0;
 
-% general wind
-for i = 1:3
-    gusts(i) = randn;
-end
-
-if mod(t,period) == 0
-    if randn > 0
-        select = rand;
-        if select < .3333
-            idx = 1;
-        elseif select > .6666
-            idx = 3;
-        else
-            idx = 2;
-        end % create a gust
-        gusts(idx) = normrnd(0, amplitude);
+if enabled == 1
+    % general wind
+    for i = 1:3
+        gusts(i) = randn;
     end
-   
+
+    if mod(t,period) == 0
+        if randn > 0
+            select = rand;
+            if select < .3333
+                idx = 1;
+            elseif select > .6666
+                idx = 3;
+            else
+                idx = 2;
+            end % create a gust
+            gusts(idx) = normrnd(0, amplitude);
+        end
+    end
 end
 
