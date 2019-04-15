0a4d7227723804308ee064110edda1a726fffd13
darrahts <timothy.s.darrah@vanderbilt.edu>
2019-04-15 12:54:21 -0500

    fixed merge

--- systemModels/pitchRef.m
+++ systemModels/pitchRef.m
@@ -1,10 +1,18 @@
 function pitch_out = pitchRef(t, pitch_ref)
 
 
+<<<<<<< HEAD
+    if t > 5 && t < 6
+        pitch_out = pitch_ref;
+    else
+        pitch_out = 0.0;
+    end
+=======
 if t > 5 && t < 6
     pitch_out = pitch_ref;
 else
     pitch_out = 0.0;
 end
+>>>>>>> 2f9c7fc6368d1a28ba4f84b89bc01ca4c6ffc5ed
 
 end
